8f8f2bbf44450615af82b01588989d20de3e575d
Hiro <hiro.nagami@gmail.com>
2023-03-14 07:40:32 +0900

    Refactoring test

--- tests/hooks/recoil/contents/index.test.ts
+++ tests/hooks/recoil/contents/index.test.ts
@@ -13,26 +13,25 @@ const useMock = () => {
   return { fetch, countUp }
 }
 
-describe('The form state should', () => {
+describe('', () => {
     test('test', async () => {
       const { result } = renderRecoilHook(useMock)
-      // await flushPromisesAndTimers()
 
       await act(async () => {
-        expect(await result.current.fetch('key1')).toEqual({ data: 'data-key1-0' })
-        
+        expect(result.current.fetch('key1')).resolves.toEqual({ data: 'data-key1-0' })
+
         await result.current.countUp('key1')
-        expect(await result.current.fetch('key1')).toEqual({ data: 'data-key1-1' })
+        expect(result.current.fetch('key1')).resolves.toEqual({ data: 'data-key1-1' })
 
-        expect(await result.current.fetch('key2')).toEqual({ data: 'data-key2-0' })
-
-        await result.current.countUp('key2')
-        expect(await result.current.fetch('key2')).toEqual({ data: 'data-key2-1' })
+        expect(result.current.fetch('key2')).resolves.toEqual({ data: 'data-key2-0' })
 
         await result.current.countUp('key2')
-        expect(await result.current.fetch('key2')).toEqual({ data: 'data-key2-2' })
+        expect(result.current.fetch('key2')).resolves.toEqual({ data: 'data-key2-1' })
 
-        expect(await result.current.fetch('key1')).toEqual({ data: 'data-key1-1' })
+        await result.current.countUp('key2')
+        expect(result.current.fetch('key2')).resolves.toEqual({ data: 'data-key2-2' })
+
+        expect(result.current.fetch('key1')).resolves.toEqual({ data: 'data-key1-1' })
     })
 
     }, 30000)
