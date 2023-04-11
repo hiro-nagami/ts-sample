16787796d6b298ee5ba0076557cd121e0964a3df
Hiro <hiro.nagami@gmail.com>
2023-04-11 10:38:21 +0900

    Add record samples

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,34 @@
 export const range = (begin: number, end: number) => ([...Array(end - begin)].map((_, i) => (begin + i)))
+
+export const isDeepEqual = (object1: Record<string, any>, object2: Record<string, any>) => {
+
+    const objKeys1 = Object.keys(object1);
+    const objKeys2 = Object.keys(object2);
+  
+    if (objKeys1.length !== objKeys2.length) { return false }
+  
+    for (var key of objKeys1) {
+      const value1 = object1[key];
+      const value2 = object2[key];
+      const isObjects = isObject(value1) && isObject(value2);
+  
+      if (isObjects && !isDeepEqual(value1, value2)) {
+        return false
+      }
+  
+      if (!isObjects && value1 !== value2) {
+        return false
+      }
+    }
+  
+    return true;
+  }
+  
+  const isObject = (object: any) => {
+    return isNonNull(object) && typeof object === "object";
+  }
+
+  export const isNonNull = (object: any) => {
+    return object !== null && object !== undefined
+}
+
